0bf5a2c898144afc9b1726dd6513278c104c5351
df <duanfeisj@gmail.com>
2013-07-17 15:01:49 +0800

    格式化

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 var fs = require('fs'),
-    http = require('http'),
-    sio = require('socket.io'),
-    parseCookie = require('connect').utils.parseCookie;
-    online = require('./online');
+http = require('http'),
+sio = require('socket.io'),
+parseCookie = require('connect').utils.parseCookie;
+online = require('./online');
 
 
 var server = http.createServer(function(req, res) {
 
-  console.log( req.session);
-  res.writeHead(200, { 'Content-type': 'text/html'});
-  res.end(fs.readFileSync('./index.html'));
+    console.log( req.session);
+    res.writeHead(200, {
+        'Content-type': 'text/html'
+    });
+    res.end(fs.readFileSync('./index.html'));
 });
 server.listen(8000, function() {
-  console.log('Server listening at http://localhost:8000/');
+    console.log('Server listening at http://localhost:8000/');
 });
 // Attach the socket.io server
 io = sio.listen(server);
@@ -25,32 +27,32 @@ io = sio.listen(server);
 var messages = ['wellcome !'];
 // Define a message handler
 io.sockets.on('connection', function (socket) {
-  socket.on('message', function (msg) {
-    //console.log('Received: ', msg);
-    messages.push(msg);
+    socket.on('message', function (msg) {
+        //console.log('Received: ', msg);
+        messages.push(msg);
 
-    console.log('arguments', arguments);
+        console.log('arguments', arguments);
 
-    socket.broadcast.emit('message', msg);
-  });
-
-
-  socket.on('online', function (userId, fn) {
-
-    console.log('userId', userId);
-
-    //online.addUser(userId);
-
-    online.getOnlineUser(fn);
+        socket.broadcast.emit('message', msg);
+    });
 
 
-    online.sendNewUser(userId, socket);
+    socket.on('online', function (userId, fn) {
 
-  });
+        console.log('userId', userId);
+
+        //online.addUser(userId);
+
+        online.getOnlineUser(fn);
 
 
-  // send messages to new clients
-  messages.forEach(function(msg) {
-    socket.send(msg);
-  })
+        online.sendNewUser(userId, socket);
+
+    });
+
+
+    // send messages to new clients
+    messages.forEach(function(msg) {
+        socket.send(msg);
+    })
 });
